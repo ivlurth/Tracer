400b1a23b8f94b0712b0d826e00bf4c294c10432
ivlurth <52335508+ivlurth@users.noreply.github.com>
2022-01-12 18:42:32 +0300

    test solution and serilization

---
+++ TracerSerialize/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TracerSerialize
+{
+    class FileWriter : IWriter
+    {
+        string path;
+
+        public FileWriter(string path)
+        {
+            this.path = path;
+        }
+
+        public void Write(string text)
+        {
+            File.WriteAllText(path, text);
+        }
+    }
+}
